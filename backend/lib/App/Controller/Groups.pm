e427c78e4594528b9e7c4da3a24ec942d4d596d3
Yegor <bravo13blog@gmail.com>
2020-07-30 02:39:47 +0300

    Forgotten edit group method

--- backend/lib/App/Controller/Groups.pm
+++ backend/lib/App/Controller/Groups.pm
@@ -7,7 +7,7 @@ use Dancer2::Plugin::DBIC;
 use Dancer2::Plugin::Routes;
 use Dancer2::Plugin::Permissions;
 
-register_permission 'groups_list', 'create_group';
+register_permission 'groups_list', 'create_group', 'edit_group', 'delete_group';
 
 register_route {
     method => 'post',
@@ -26,7 +26,7 @@ register_route {
 register_route {
     method => "post",
     regexp => "/groups/add",
-    code => has_permission 'add_group', sub {
+    code => has_permission 'create_group', sub {
         my $group_data = request->data->{group};
         my $groups_model = rset('Group');
         my $validator = $groups_model->validator();
@@ -40,6 +40,43 @@ register_route {
             }
         } else {
             $groups_model->create($valid_data);
+            return {
+                success => 1
+            }
+        }
+    }
+};
+
+register_route {
+    method => 'post',
+    regexp => '/groups/edit',
+    code => has_permission 'edit_group', sub {
+        my $group_data = request->data->{group};
+        my $groups_model = rset('Group');
+        my $validator = $groups_model->validator( skip_name => 1 );
+        my $valid_data = $validator->validate( $group_data );
+        my $errors = $validator->get_errors();
+
+        if( $errors ){
+            return {
+                error => "Failed params",
+                params => $errors
+            }
+        } else {
+            my $name = delete $group_data->{name};
+            unless(keys(%{$group_data})){
+                return {
+                    error => "Nothing to update"
+                }
+            }
+            my $group = $groups_model->find({name => $name});
+            unless($group){
+                return {
+                    error => "Group not found"
+                }
+            }
+
+            $group->update($group_data);
             return {
                 success => 1
             }
@@ -76,6 +113,22 @@ register_route {
         return {
             success => 1
         }
+    }
+};
+
+register_route {
+    method => "post",
+    regexp => "/group/permissions/list",
+    code => has_permission 'edit_group', sub {
+
+    }
+};
+
+register_route {
+    method => "post",
+    regexp => "/group/permissions/set",
+    code => has_permission 'edit_group', sub {
+
     }
 };
 
